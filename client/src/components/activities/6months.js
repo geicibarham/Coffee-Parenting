48332df412ab621e350e3707589e1f5c2cb54036
unknown <geicianecosta43@gmail.com>
2022-06-15 16:20:13 -0400

    six months old babies page

--- client/src/components/activities/6months.js
+++ client/src/components/activities/6months.js
@@ -1,7 +1,49 @@
 
+import tummy from '../../assets/images/tummy.jpg'
+import play from '../../assets/images/bed.jpg'
+import css from './style.css'
+import Footer from '../footer'
+
 function Sixmonths() {
     return (
-        <h1>test</h1>
+        <>
+            <h3 style={{ textAlign: "center" }}>Young Infants</h3>
+            <section id="six-month-container">
+
+                <div id="text-image">
+                    <h4 style={{ width: "50%", 
+                    alignSelf: "center",
+                    color:"black",
+                    fontWeight:"lighter" }}>
+                        At this age it is very important to strengthen
+                        core and neck muscles, as that will be the foundation
+                        for sitting,
+                        crawling, walking and will help improve motor skills.
+                    </h4>
+                    < img style={{ width: "400px",  borderRadius: "20px" }} 
+                    src={tummy} alt="baby doing tummy time" />
+                </div>
+
+                <div id="image">
+                    < img style={{ width: "400px", borderRadius: "20px" }}
+                        src={play} alt="baby doing tummy time" />
+
+                    <h4 style={{
+                    
+                        width: "50%", alignSelf: "center",
+                        color:"black",
+                        fontWeight:"lighter"
+                    }}>
+                        Some tips and tricks for making tummy time more enjoyable
+                        for baby is reading books, listening to songs and traveling tummy time.
+                        To do traveling tummy time, simply place baby facing down on your
+                        arms and walk around the house, telling baby a story, singing on
+                        simply describe what you see.
+                    </h4>
+                </div>
+            </section>
+            < Footer />
+        </>
     )
 }
 
