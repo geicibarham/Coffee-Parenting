869788662e478c3887bd80e580ec0b169f534000
unknown <geicianecosta43@gmail.com>
2022-06-19 13:25:08 -0400

    heroku errors

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/checkout-my-code",
+  process.env.MONGODB_URI || "mongodb://localhost/checkout-my-code",
   {
     useUnifiedTopology: true,
     useNewUrlParser: true, 
