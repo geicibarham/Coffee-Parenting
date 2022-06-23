951626c7991ea0b2c73301dd42d75510bb2e66f7
geici <geicianecosta43@gmail.com>
2022-06-23 14:10:49 -0400

    boys names work, need to add gender neutral names

--- client/src/components/friend-list/index.js
+++ client/src/components/friend-list/index.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import girlNames from '../../data.json'
+import boyNames from '../../data.boys.json'
+import neutralNames from '../../data.neutral.json'
 import css from './name.css'
 import baby from '../../assets/images/baby.png'
 import girl from '../../assets/images/girl.png'
 import boy from '../../assets/images/boy.png'
+
 import neutral from '../../assets/images/neutral.png'
 function Names() {
 
@@ -11,11 +14,23 @@ function Names() {
   const [randomElement, setrandomElement] =
     useState('');
 
-    const [meaning, setMeaning] =
+  const [meaning, setMeaning] =
     useState('');
 
   function getRandom() {
     let randomElement = girlNames[Math.floor(Math.random() * girlNames.length)];
+    console.log(randomElement)
+    setrandomElement(randomElement)
+    setMeaning(randomElement.meaning)
+  }
+  function getRandomboys() {
+    let randomElement = boyNames[Math.floor(Math.random() * boyNames.length)];
+    console.log(randomElement)
+    setrandomElement(randomElement)
+    setMeaning(randomElement.meaning)
+  }
+  function getRandomneutral() {
+    let randomElement = neutralNames[Math.floor(Math.random() * neutralNames.length)];
     console.log(randomElement)
     setrandomElement(randomElement)
     setMeaning(randomElement.meaning)
@@ -32,28 +47,34 @@ function Names() {
         </h4>
 
 
-          <div className='button-container'>
-            <button id="girl"
+        <div className='button-container'>
+          <button id="girl"
             onClick={getRandom} className="btn btn-info 
             button-style">Girls
-            <img style={{height:"30px"}}src ={girl}/>
-            </button>
-            <button id="boy" onClick={getRandom} 
-            className="btn btn-info button-style">Boys
-             <img alt="boy icon" style={{height:"30px",
-             paddingLeft:"5px"
-            }}src ={boy}/></button>
+            <img style={{ height: "30px" }} src={girl} />
+          </button>
 
-            <button id="neutral"onClick={getRandom}
+          <button id="boy" onClick={getRandomboys}
+            className="btn btn-info button-style">Boys
+            <img alt="boy icon" style={{
+              height: "30px",
+              paddingLeft: "5px"
+            }} src={boy} /></button>
+
+          <button id="neutral" onClick={getRandomneutral}
             className="btn btn-info button-style">Neutral
-            <img style={{height:"30px"}} alt="neutral icon"
-            src={neutral} /></button>
-          </div>
-          {/* <button onClick={getRandom}>Test</button> */}
-          <div className='outer-container'>
+            <img style={{ height: "30px" }} alt="neutral icon"
+              src={neutral} />
+          </button>
+        </div>
+
+        <div className='outer-container'>
           <div className='name-container'>
-          <h1>{randomElement.name}</h1> <br/>
-          <p style={{textAlign:"center"}}> {meaning}</p>
+            <h1>{randomElement.name}</h1> <br />
+
+          </div>
+          <div className='meaning-container'>
+            <p style={{ width: "50%", textAlign: "center" }}> {meaning}</p>
           </div>
 
         </div>
