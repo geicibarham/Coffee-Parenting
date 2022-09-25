1f6b6071994a2164c5c8f0ff1b48d99148f9e0aa
geici <geicianecosta43@gmail.com>
2022-09-25 19:49:10 -0400

    more changes on style

--- client/src/components/activities/6months.js
+++ client/src/components/activities/6months.js
@@ -7,103 +7,73 @@ import Footer from '../footer'
 import { Link } from 'react-router-dom'
 import React from 'react'
 function Sixmonths() {
-    return ( <>
+    return (<>
 
 
-            <h3 style={{
-                textAlign: "center",
-                paddingBottom: "20px",
-                paddingBottom: "20px"
-            }}>Young Infants</h3>
+        <h1> Young Infants </h1>
+        <section id="six-month-container">
+            <div id="first_container">
 
-        
-            <section id="six-month-container">
-          
-          
-                <div id="text-image">
+                <ul id="activity-list">
 
-                <ul style={{width:"15%",height:"fit-content",
-            alignSelf:"center",
-            marginRight:"20px"}} id="activity-list">
 
-                
                     <Link to="/one-three">1-3 Years</Link>  < br /> < br />
                     <Link to="/three">3+ Years</Link>  < br /> < br />
 
 
 
                 </ul>
-                    <h4 style={{
-                        width: "50%",
-                        alignSelf: "center",
-                        color: "black",
-                        fontWeight: "lighter"
-                    }}>
-                        At this age it is very important to strengthen
-                        core and neck muscles, as that will be the foundation
-                        for sitting,
-                        crawling, walking and will help improve motor skills.
-                    </h4>
-                    < img style={{
-                        width: "400px",
-                        borderRadius: "20px",
-                        marginLeft: "20px"
-                    }}
-                        src={tummy} alt="baby doing tummy time" />
-                </div>
-                <h4 style={{
-                    textAlign: "center",
-                    paddingTop: "20px"
-                }}>Tips and Tricks</h4>
-                <div id="image">
+                <h4>
+                    At this age it is very important to strengthen
+                    core and neck muscles, as that will be the foundation
+                    for sitting,
+                    crawling, walking and will help improve motor skills.
+                </h4>
+                < img id="baby_first_row" style={{
 
-                    < img style={{
-                        width: "400px", borderRadius: "20px",
-                        marginRight: "20px"
-                    }}
-                        src={play} alt="baby doing tummy time" />
+                }}
+                    src={tummy} alt="baby doing tummy time" />
+            </div>
 
-                    <h4 style={{
+            <h4 className='title'>Tips and Tricks</h4>
+            <div id="second_row">
 
-                        width: "50%", alignSelf: "center",
-                        color: "black",
-                        fontWeight: "lighter"
-                    }}>
-                        Some tips and tricks for making tummy time more enjoyable
-                        for baby is reading books, listening to songs and traveling tummy time.
-                        To do traveling tummy time, simply place baby facing down on your
-                        arms and walk around the house, telling baby a story, singing on
-                        simply describe what you see.
-                    </h4>
-                </div>
+                < img style={{
+                    width: "400px", borderRadius: "20px",
+                    marginBottom: "20px",
+                    marginLeft: "10px"
+                }}
+                    src={play} alt="baby doing tummy time" />
+
+                <h4 >
+                    Some tips and tricks for making tummy time more enjoyable
+                    for baby is reading books, listening to songs and traveling tummy time.
+                    To do traveling tummy time, simply place baby facing down on your
+                    arms and walk around the house, telling baby a story, singing on
+                    simply describe what you see.
+                </h4>
+            </div>
 
 
-                <h4 style={{
-                    textAlign: "center",
-                    paddingTop: "20px",
-                    paddingBottom: "20px"
-                }}>Tummy Time and Water</h4>
-                <div id="text-image">
-                    <h4 style={{
-                        width: "50%",
-                        alignSelf: "center",
-                        color: "black",
-                        fontWeight: "lighter"
-                    }}>
-                        Babies love water!
-                        Put water on a baking sheet with baby's favorite toys and
-                        let them have fun!
-                    </h4>
-                    < img style={{
-                        width: "400px",
-                        borderRadius: "20px",
-                        marginLeft: "20px"
-                    }}
-                        src={water} alt="baby doing tummy time" />
-                </div>
-            </section>
-            < Footer />
-        </>
+            <h4 className="title">
+                Tummy Time and Water</h4>
+
+            <div id="last_row">
+                <h4>
+                    Babies love water!
+                    Put water on a baking sheet with baby's favorite toys and
+                    let them have fun!
+                </h4>
+                < img style={{
+                    width: "400px",
+                    borderRadius: "20px",
+                    marginLeft: "20px"
+                }}
+                    src={water} alt="baby doing tummy time" />
+            </div>
+        </section>
+        < Footer />
+    </>
     )
 }
 
--- client/src/components/activities/index.js
+++ client/src/components/activities/index.js
@@ -11,8 +11,10 @@ function Activities() {
                 paddingTop: "10px"
             }}>
                 Create Memories!</h3>
-            <section id="activity-container">
-                
+            <section id="outer_container">
+
+
+
                 <ul id="activity-list">
 
                     <Link to="/sixmonths">0-6 Months</Link> < br /> < br />
@@ -20,35 +22,35 @@ function Activities() {
                     <Link to="/three">3+ Years</Link>  < br /> < br />
 
                 </ul>
-                
 
-                <p style={{ width: "40%", lineHeight: "40px", margin: "auto" }}>
+
+                <p id="description">
                     From the very days of our childhood, we still remember the cookies baked by mom, dad, or grandma.
                     We can almost smell the dinner being cooked while we ran around the house, and we can almost feel
-                    the tickles from dad or grandpa. < br />
+                    the tickles from dad or grandpa. < br /> < br />
                     Creating great memories fosters connection and boosts development and we are here to help.
                     Check our list of activities and have fun!
                     On a personal note, we would like to tell you how great of a parent you are, for caring, for loving your children
                     and doing what is best for them.
-                  
+
                 </p>
-                <img style={{
+                <img id="kid_playing" style={{
                     width: "350px",
                     height: "350px",
-                    marginRight: "10px",
+                    
                     borderRadius: "20px",
-                  
+
                     marginBottom: "40px"
                 }}
                     src={play} alt="child exploring outside" />
-                
-              
-             
+
+
+
+
 
 
 
             </section>
-
             <Footer />
         </>
     )
